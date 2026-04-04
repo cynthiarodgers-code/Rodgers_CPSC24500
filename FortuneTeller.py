866004e486c9cd5feff5e68f544dcad32351262a
Cynthia Rodgers <cynthia.rodgers@lewisu.edu>
2026-04-04 15:26:31 -0500

    Second part
    Second part of assignment

--- FortuneTeller.py
+++ FortuneTeller.py
@@ -1,5 +1,15 @@
-# Fortune Teller
-
 print('*-*-*-*-*-*')
 print('Welcome to the Fortune Teller!')
 print('*-*-*-*-*-*')
+
+name = input("Please enter your full name (first and last name): ")
+age = input("Please enter your age (a postive integer): ")
+
+try:
+    age = int(age)
+    if age <= 0:
+        print("Age must be a positive integer.")
+except ValueError:
+    print("Invalid age input. Please enter an integer.")
+
+color = input("Please enter your favorite color: ")
