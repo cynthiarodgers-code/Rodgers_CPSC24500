c95efa3b349ea7fbad009cd1b3f8e5fdb1f4028d
Cynthia Rodgers <cynthia.rodgers@lewisu.edu>
2026-04-12 17:08:06 -0500

    Create menu_item.py
    Initial add

---
+++ menu_item.py
@@ -0,0 +1,49 @@
+"""
+menu_item.py - Week 3 
+
+The MenuItem class represents a single item on the coffee shop menu.
+
+Attributes:
+- name (str): drink name (e.g., "Latte")
+- size (str): "Small", "Medium", or "Large"
+- price (float): final price (base price + size upcharge)
+
+Methods:
+- __str__: returns a string like "Latte (Large) - $6.00"
+"""
+
+
+class MenuItem:
+
+    def __init__(self, name, size, price):
+        # TODO: store name, size, and price as instance attributes
+        self.name = name
+        self.size = size
+        self.base_price = self.base_price
+        self.price = self.calculate_price() 
+        pass
+    
+    def calculate_price(self):
+        upcharges = {"Small": 0.00, "Medium": 0.75, "Large": 1.25}
+        size_upcharge = upcharges.get(self.size, 0.00)
+        return self.base_price + size_upcharge
+
+
+    def __str__(self):
+        # TODO: return a string in the format "Latte (Large) - $6.00"
+        # Hint: f"{self.name} ({self.size}) - ${self.price:.2f}"
+        return f"{self.name} ({self.size}) - ${self.price:.2f}"
+        pass
+    
+    item1 = MenuItem("Americano", "Small", 3.50)
+    item2 = MenuItem("Cappucino", "Small", 4.25)
+    item3 = MenuItem("Espresso", "Small",3.00)
+    item4 = MenuItem("Latte", "Small", 4.75)
+
+    menu = [item1, item2, item3, item4]
+
+    for item in menu:
+        print(item)
+
+
+    
