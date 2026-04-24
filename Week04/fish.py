baa0717d267fdb52c3a94f3eb152f9582d510988
Cynthia Rodgers <cynthia.rodgers@lewisu.edu>
2026-04-23 20:16:36 -0500

    Create fish.py
    initial code entry

---
+++ Week04/fish.py
@@ -0,0 +1,30 @@
+"""
+fish.py - Week 5 Starter
+
+Fish extends Pet.
+- Override feed() and play()
+- Also override sleep() (fish don't sleep the same way)
+"""
+
+from pet import Pet
+
+
+class Fish(Pet):
+
+    def __init__(self, name):
+        # TODO: call super().__init__(name, "Fish")
+        super().__init__(name, "Fish")
+
+    def feed(self):
+        # TODO: fish-specific feeding
+        self._hunger += 5
+        self._energy += 10
+
+    def play(self):
+        # TODO: fish-specific play
+        self._happiness += 10
+        self._energy -= 5
+
+    def sleep(self):
+        # TODO: fish-specific sleep behavior (different from base class)
+        return f"{self._name} swims sleepily."
